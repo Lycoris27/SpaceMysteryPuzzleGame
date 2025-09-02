aa61d2c4e1d2f9525858c65b1dab949e1bcc60b3
EPT54321 <62244130+EPT54321@users.noreply.github.com>
2025-09-02 20:49:24 +0800

    Pipe section nearlly done
    - For some reason the start and end wont show up as different tiles even though I set numConn = 1.
    - Test DFS of the structure (by pressing E on the sphere)

--- Assets/Scripts/Ethan's Scripts/PipePuzzle.cs
+++ Assets/Scripts/Ethan's Scripts/PipePuzzle.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using NUnit.Framework.Internal;
+using Unity.Services.Core;
+using Unity.VisualScripting;
 using UnityEditor.Animations;
 using UnityEngine;
 
@@ -14,9 +16,106 @@ public class PipePuzzle : MonoBehaviour
     [SerializeField] public float rz = 1.0f;
     [SerializeField] public GameObject loadedPrefab; 
     [SerializeField] public GameObject[] grid;
+    [SerializeField] public float interactionDist;
+
+    public void updateState(PipeSection sec)
+    {
+        if (sec.activeObj != null)
+        {
+            sec.activeObj.SetActive(false);
+            sec.transform.GetChild(0).gameObject.SetActive(true);
+        }
+
+        sec.activeObj = sec.transform.GetChild(sec.numConn).gameObject;
+        sec.activeObj.SetActive(true);
+
+        //Reset rotation
+        sec.rot = 0.0f;
+        updateRot(sec);
+    }
+
+    public void updateRot(PipeSection sec)
+    {
+        sec.activeObj.transform.eulerAngles = new Vector3(sec.activeObj.transform.rotation.x, sec.rot, sec.activeObj.transform.rotation.z);
+
+        //Update connections list
+        sec.conn.Clear();
+
+        /*              0
+         *            3 x 1  Connection directions
+         *              2
+         *              
+         *              sec.numConn
+         *              - 0 = empty
+         *              - 1 = line
+         *              - 2 = bend
+         *              - 3 = T section
+         *              - 4 = all
+         *              
+         */
+
+        int conn1,conn2,conn3;
+        switch(sec.numConn)
+        {
+            case 0: // Empty
+                break;
+            case 1: // Straight
+                conn1 = (0 + (int)(sec.rot / 90.0f)) % 4;
+                conn2 = (2 + (int)(sec.rot / 90.0f)) % 4;
+                sec.conn.Add(conn1);
+                sec.conn.Add(conn2);
+                break;
+            case 2: //Bend
+                conn1 = (1 + (int)(sec.rot / 90.0f)) % 4;
+                conn2 = (2 + (int)(sec.rot / 90.0f)) % 4;
+                sec.conn.Add(conn1);
+                sec.conn.Add(conn2);
+                break;
+            case 3:
+                conn1 = (0 + (int)(sec.rot / 90.0f)) % 4;
+                conn2 = (1 + (int)(sec.rot / 90.0f)) % 4;
+                conn3 = (2 + (int)(sec.rot / 90.0f)) % 4;
+                sec.conn.Add(conn1);
+                sec.conn.Add(conn2);
+                sec.conn.Add(conn3);
+                break;
+            case 4:
+                sec.conn.Add(0);
+                sec.conn.Add(1);
+                sec.conn.Add(2);
+                sec.conn.Add(3);
+                break;
+        }
+
+        //Debug.Log(string.Join(" ", sec.conn));
+    }
+
+    public void initPipeState(PipeSection sec)
+    {
+        sec.numConn = 0;
+        sec.accessed = false;
+        foreach (Transform child in sec.transform)
+        {
+            GameObject childObject = child.gameObject;
+            childObject.SetActive(false);
+        }
+
+        GameObject BasePlate = sec.transform.GetChild(0).gameObject;
+        BasePlate.SetActive(true);
+
+        //Temp start and end values might have a different visual object to represent
+        if (sec.isEnd || sec.isStart)
+        {
+            sec.numConn = 1;
+        }
+
+        updateState(sec);
+    }
 
     private void Start()
     {
+        interactionDist = 10.0f;
+
         playerCamera = GameObject.Find("PlayerCameraRoot").GetComponent<Camera>();
 
         //Generate the pipe puzzle originating from this objects position
@@ -35,7 +134,8 @@ public class PipePuzzle : MonoBehaviour
                 //Set position
                 grid[idx].transform.position = origin + new Vector3(rx*j, 0, rz*i);
                 PipeSection sec = grid[idx].GetComponent<PipeSection>();
-                sec.playerCamera = playerCamera;
+
+                initPipeState(sec);
             }
         }
 
@@ -47,13 +147,137 @@ public class PipePuzzle : MonoBehaviour
     }
 
     //Depth-First search of the arrays
-    public void DFS()
+    public bool DFS(PipeSection pipe, int grididx, int depth)
     {
+        if (pipe.isEnd)
+        {
+            return true;
+        }
 
+        pipe.accessed = true;
+
+        //North
+        if (grididx >= gridz && pipe.conn.Contains(0))
+        {
+            GameObject north = grid[grididx - gridz];   
+            PipeSection northPS = north.GetComponent<PipeSection>();
+
+            if (northPS.accessed == false && northPS.conn.Contains(2))
+            {
+                if (DFS(northPS, grididx - gridz, depth + 1)) return true;
+            }
+
+        }
+        //East
+        if ((grididx+1)%gridx == 0 && pipe.conn.Contains(1))
+        {
+            GameObject east = grid[grididx + 1];
+            PipeSection eastPS = east.GetComponent<PipeSection>();  
+
+            if (eastPS.accessed == false && eastPS.conn.Contains(3))
+            {
+                if(DFS(eastPS, grididx + 1, depth + 1)) return true;
+            }
+        }
+
+        //South
+        if (grididx >= (gridz-1)*gridx && pipe.conn.Contains(2))
+        {
+            GameObject south = grid[grididx + gridz];
+            PipeSection southPS = south.GetComponent<PipeSection>();
+
+            if (southPS.accessed == false && southPS.conn.Contains(0))
+            {
+                if (DFS(southPS, grididx + gridz, depth + 1)) return true;
+            }
+        }
+
+        //West
+        if (grididx % gridx == 0 && pipe.conn.Contains(3))
+        {
+            GameObject west = grid[grididx - 1];
+            PipeSection westPS = west.GetComponent<PipeSection>();
+
+            if (westPS.accessed == false && westPS.conn.Contains(1))
+            {
+                if (DFS(westPS, grididx - 1, depth + 1)) return true;
+            }
+        }
+
+        return false;
     }
 
     private void Update()
     {
-        
+        //Change pipe type
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, interactionDist))
+            {
+                if (hit.collider.gameObject.name == "Base-Plate")
+                {
+                    PipeSection par = hit.collider.gameObject.GetComponentInParent<PipeSection>();
+                    par.numConn += 1;
+                    if (par.numConn > 4) par.numConn = 0; //5 different pipes reset after 5th
+                    updateState(par);
+                }
+
+                if (hit.collider.gameObject.name == "Pressure-Button")
+                {
+                    GameObject startObj = null;
+                    int finalidx = -1;
+                    //Loop through the grid to find the start
+                    for(int i = 0; i < gridz; i++)
+                    {
+                        for(int j = 0; j < gridx; j++)
+                        {
+                            int idx = i * gridz + j;
+                            if (grid[idx].GetComponent<PipeSection>().isStart)
+                            {
+                                startObj = grid[idx];
+                                finalidx = idx;
+                                break;
+                            }
+                        }
+                        if (startObj != null)
+                        {
+                            if (DFS(startObj.GetComponent<PipeSection>(), finalidx, 0))
+                            {
+                                Debug.Log("Found End!");
+                            }
+                            else
+                            {
+                                Debug.Log("No link to End!");
+                            }
+
+                            break;
+                        }
+                    }
+                    if (startObj == null)
+                    {
+                        Debug.Log("Error: Couldn't find start pipe");
+                    }
+                }
+            }
+        }
+
+        //Rotate pipe
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            RaycastHit hit;
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, interactionDist))
+            {
+                if (hit.collider.gameObject.name == "Base-Plate")
+                {
+                    PipeSection par = hit.collider.gameObject.GetComponentInParent<PipeSection>();
+                    par.rot += 90.0f;
+                    if (par.rot > 270.0f) par.rot = 0.0f; //4 directions reset after 3rd
+                    updateRot(par);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Ethan's Scripts/PipeSection.cs
+++ Assets/Scripts/Ethan's Scripts/PipeSection.cs
@@ -1,51 +1,15 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 public class PipeSection : MonoBehaviour
 {
-    [SerializeField] public Camera playerCamera;
     [SerializeField] public GameObject activeObj;
     [SerializeField] public bool isStart = false;
     [SerializeField] public bool isEnd = false;
-    [SerializeField] public int numConn = 0;
-    [SerializeField] public float rot = 0.0f;
-    [SerializeField] public float interactionDist = 1.0f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        initState();
-    }
-
-    public void initState()
-    {
-        foreach(Transform child in transform)
-        {
-            GameObject childObject = child.gameObject;
-            childObject.SetActive(false);
-        }
-
-        GameObject BasePlate = transform.GetChild(0).gameObject;
-        BasePlate.SetActive(true);
-
-        activeObj = transform.GetChild(numConn).gameObject;
-        activeObj.SetActive(true);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            foreach (Transform obj in activeObj.GetComponentInChildren<Transform>())
-            {
-                //Do Raycast to check main camera staring at object
-                RaycastHit hit;
-                if (Physics.Raycast(playerCamera.transform.position, (obj.position - playerCamera.transform.position).normalized, out hit, interactionDist))
-                {
-
-                }
-            }
-            
-        }
-    }
+    [SerializeField] public int numConn;
+    [SerializeField] public float rot;
+    [SerializeField] public List<int> conn;
+    [SerializeField] public bool accessed; //for DFS
 }
